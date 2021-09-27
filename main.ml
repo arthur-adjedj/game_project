5ba36a19071e3e1ebb42207f1d27c53412a92536
arthur-adjedj <aarthuur01@gmail.com>
2021-09-27 12:17:45 +0200

    fixed collisions + added game_over screen

--- main.ml
+++ main.ml
@@ -2,6 +2,7 @@ open Graphics
 open Plane
 open Buildings
 open Bombs
+open Score
 
 let game_over = ref false
 
@@ -27,6 +28,7 @@ let background () = make_image (Array.make_matrix 800 800 white)
 
 
 let next_tick () = 
+  set_color black;
   fill_rect 0 0 800 800;
   update_plane ();
   update_bombs ();
@@ -34,9 +36,11 @@ let next_tick () =
   draw_bombs ();
   draw_plane ();
   draw_buildings ();
+  draw_score ();
   if has_crashed () then game_over := true
 
 
+exception Game_Over
 
 
 let () = open_graph "800x800";
@@ -48,16 +52,24 @@ let () = open_graph "800x800";
   (*uwu*)
   uwu;
 
-  while true (*not !game_over*) do 
+  while not !game_over do 
+    let t = Sys.time () in
     if button_down () then add_bomb ();
       synchronize ();
       next_tick ();
+    wait (max 0. (300. -. (Sys.time () -. t) ))
   done ;
 
-  print_string "game over :";
+  
+
   set_color red;
-  draw_image (make_image (Array.make_matrix 80 80 red)) 300 300;
+  fill_rect 0 0 800 800;
+  moveto 350 400;
+  set_text_size 5;
+  set_color black;
+  draw_string "GAME OVER";
+  synchronize ();
 
   while true do
-    ()
+    plot 0 0
   done
--- plane.ml
+++ plane.ml
@@ -6,11 +6,11 @@ exception Break
 
 (*whether this func workswith the bot_left origin system of Graphics needs to be verified*)
 (*PRIORITY*)
-let are_in_collision _ _ _ _(*(x1,y1) (w1,h1) (x2,y2) (w2,h2)*) = false 
-    (*not ((x1 > x2 + w2 ||
-     x2 > x1 + w1) &&
-    (y1 > y2 + h2 ||
-     y2 > y1 + h1))*)
+let are_in_collision (x1,y1) (w1,h1) (x2,y2) (w2,h2) = 
+    not (x1 > x2 + w2 ||
+     x2 > x1 + w1 ||
+     y1 > y2 + h2 ||
+     y2 > y1 + h1)
 
 let iof = int_of_float 
 
@@ -27,7 +27,7 @@ let draw_plane () =
 
 let update_plane () = 
     pos := (((fst !pos) + iof (fst  !vel)) mod 800,((snd !pos) + iof (snd !vel)) mod 800);
-    vel := (fst !vel +. 1., snd !vel) 
+    vel := (fst !vel , snd !vel) 
 
 
 let has_crashed () = 
@@ -35,7 +35,7 @@ let has_crashed () =
     try
         for k = 0 to n_of_buildings - 1 do 
             let h = b_heights.(k) in
-           if  are_in_collision !pos (30,50) (k*(block_width+40),0) ((h*block_height),block_width) then 
+            if  are_in_collision !pos (30,50) (k*(block_width+40),0) (block_width,(h*block_height)) then 
                 (res:=true;
                 raise Break)
         done;
